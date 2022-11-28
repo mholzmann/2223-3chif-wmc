4ed055b15778e5ebb629cac48a53b940341eb222
mholzmann <m.holzmann@htl-leonding.ac.at>
2022-11-28 11:22:01 +0100

    remove exercises

--- 08_asynchronous-programming/exercises/02_client-clock/solution/public/client-clock.js
+++
@@ -1,9 +0,0 @@
-var tickTack = document.getElementById('tick-tack');
-setInterval(function () {
-    if (tickTack.innerText === 'tick') {
-        tickTack.innerText = 'tack';
-    }
-    else {
-        tickTack.innerText = 'tick';
-    }
-}, 1000);
--- 08_asynchronous-programming/exercises/02_client-clock/solution/public/client-clock.ts
+++
@@ -1,9 +0,0 @@
-const tickTack = <HTMLHeadingElement> document.getElementById('tick-tack');
-
-setInterval(() => {
-    if (tickTack.innerText === 'tick') {
-        tickTack.innerText = 'tack';
-    } else {
-        tickTack.innerText = 'tick';
-    }
-}, 1000);
