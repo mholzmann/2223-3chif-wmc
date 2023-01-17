b4e264660331b451777967845d5398f48b200cee
mholzmann <m.holzmann@htl-leonding.ac.at>
2023-01-17 13:08:48 +0100

    simplify demo

--- 10_file-system/04_callbacks-vs-promises/merge-with-callbacks.ts
+++ 10_file-system/04_callbacks-vs-promises/merge-with-callbacks.ts
@@ -1,9 +1,12 @@
 import { readFile, writeFile } from 'fs';
 import { join } from 'path';
 
-// Welcome in the 'callback hell' ...
-console.log('reading part1.txt ...');
 const path1 = join(__dirname, 'files', 'part1.txt');
+const path2 = join(__dirname, 'files', 'part2.txt');
+const pathMerged = join(__dirname, 'files', 'merged-with-callbacks.txt');
+
+// Welcome in the 'callback hell'
+console.log('reading part1.txt ...');
 readFile(path1, 'utf8', (err, data) => {
     if (err !== null) {
         console.error(err.message);
@@ -13,7 +16,6 @@ readFile(path1, 'utf8', (err, data) => {
     console.log('read file successfully');
 
     console.log('reading part2.txt ...');
-    const path2 = join(__dirname, 'files', 'part2.txt');
     readFile(path2, 'utf8', (err, data) => {
         if (err !== null) {
             console.error(err.message);
@@ -23,7 +25,6 @@ readFile(path1, 'utf8', (err, data) => {
         console.log('read file successfully');
 
         console.log('writing file ...');
-        const pathMerged = join(__dirname, 'files', 'merged-with-callbacks.txt');
         writeFile(pathMerged, text, (err) => {
             if (err !== null) {
                 console.error(err.message);
--- 10_file-system/04_callbacks-vs-promises/merge-with-promises.ts
+++ 10_file-system/04_callbacks-vs-promises/merge-with-promises.ts
@@ -4,6 +4,10 @@
 import { readFile, writeFile } from 'fs/promises';
 import { join } from 'path';
 
+const path1 = join(__dirname, 'files', 'part1.txt')
+const path2 = join(__dirname, 'files', 'part2.txt')
+const pathMerged = join(__dirname, 'files', 'merged-with-promises.txt')
+
 mergeFiles();
 
 console.log('\n*** could do something else meanwhile ***\n');
@@ -11,17 +15,14 @@ console.log('\n*** could do something else meanwhile ***\n');
 async function mergeFiles() {
     try {
         console.log('reading part1.txt ...');
-        const path1 = join(__dirname, 'files', 'part15.txt')
         let text = await readFile(path1, 'utf8');
         console.log('read file successfully');
 
         console.log('reading part2.txt ...');
-        const path2 = join(__dirname, 'files', 'part2.txt')
         text += await readFile(path2, 'utf8');
         console.log('read file successfully');
 
         console.log('writing merged file ...');
-        const pathMerged = join(__dirname, 'files', 'merged-with-promises.txt')
         await writeFile(pathMerged, text);
         console.log('wrote file successfully');
     } catch (err: any) {
