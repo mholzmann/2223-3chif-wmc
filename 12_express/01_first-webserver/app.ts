1017719fa6e7a0427a8015ad9da715f64f64793e
mholzmann <m.holzmann@htl-leonding.ac.at>
2023-02-13 11:29:29 +0100

    fix typo

--- 12_express/01_first-webserver/app.ts
+++ 12_express/01_first-webserver/app.ts
@@ -3,30 +3,30 @@
 // for starting the server in watchmode run 'npm run server-dev'
 
 // import modules
-import express from "express";
-import { join } from "path";
+import express from 'express';
+import { join } from 'path';
 
 // create express application
 const app = express();
 
-// respond with index.html to a HTTP GET request at route "/"
-app.get("/", (req, res) => {
-    const file: string = join(__dirname, "public/index.html");
+// respond with index.html to a HTTP GET request at route '/'
+app.get('/', (req, res) => {
+    const file: string = join(__dirname, 'public', 'index.html');
     res.sendFile(file);
 });
 
-// respond with "hello john" to a HTTP GET request at route "/john"
-app.get("/john", (req, res) => {
-    res.status(200).send("hello john");
+// respond with 'hello john' to a HTTP GET request at route '/john'
+app.get('/john', (req, res) => {
+    res.status(200).send('hello john');
 });
 
-// respond with "hello <name>" to a HTTP GET request at route "/<name>" 
+// respond with 'hello <name>' to a HTTP GET request at route '/<name>' 
 // by using a route parameter for capturing the value from the URL
-app.get("/:name", (req, res) => {
+app.get('/:name', (req, res) => {
     res.send(`hello ${req.params.name}`);
 });
 
 // start http server
 app.listen(3000, () => {
-    console.log("Server listening on port 3000");
+    console.log('Server listening on port 3000');
 });
